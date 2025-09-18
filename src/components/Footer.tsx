0b7493b1431380a469dcc16053e93d6e1506696a
Prasanna Kumar B K <prasannakumarbk04@gmail.com>
2025-09-18 21:44:39 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -51,7 +51,7 @@ export const Footer: React.FC = () => {
               </div>
               <div className="flex items-center space-x-3">
                 <Phone className="h-5 w-5 text-blue-400" />
-                <span className="text-gray-300">+91 86603 35845</span>
+                <span className="text-gray-300">+918660335845</span>
               </div>
               <div className="flex items-center space-x-3">
                 <MapPin className="h-10 w-10 text-blue-400" />
